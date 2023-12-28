8f4534317f9c95c37b6c096170daede5efef15ca
Rebecca Nelson <dekarrin.irl@gmail.com>
2023-12-28 10:04:42 -0600

    commit w doccomment on AuthUserRepo

--- jeldao/jeldao.go
+++ jeldao/jeldao.go
@@ -5,6 +5,12 @@ package jeldao
 import (
 	"context"
 	"errors"
+	"fmt"
+	"net/mail"
+	"strings"
+	"time"
+
+	"github.com/google/uuid"
 )
 
 var (
@@ -13,15 +19,10 @@ var (
 	ErrDecodingFailure     = errors.New("field could not be decoded from DB storage format to model format")
 )
 
-type ID interface {
-	Value() interface{}
-	Fields() map[string]interface{}
-}
-
-type Model interface {
-	ID() ID
-	Indexed() [][]string
-	Fields() map[string]interface{}
+type Model[ID any] interface {
+	// ModelID returns a jeldao-usable ID that identifies the Model uniquely.
+	// For those fields which
+	ModelID() ID
 }
 
 // Store holds all repositories.
@@ -32,10 +33,20 @@ type Store interface {
 	// not an implementor of.
 	Repo(name string) any
 
+	// ProvidesRepos gives a list of valid names of Repos that this Store
+	// provides. Each element in the list can be passed to the Repo function to
+	// get the corresponding Repo.
+	ProvidesRepos() []string
+
+	// Close closes any pending operations on the DAO store and on all of its
+	// Repos. It performs any clean-up operations necessary and should always be
+	// called once the Store is no longer in use.
 	Close() error
 }
 
-type Repo[E Model] interface {
+// Repo is a data object repository that maps ID-typed identifiers to M-typed
+// entity models.
+type Repo[ID any, M Model[ID]] interface {
 
 	// Create creates a new model in the DB based on the provided one. Some
 	// attributes in the provided one might not be used; for instance, many
@@ -44,9 +55,155 @@ type Repo[E Model] interface {
 	// are used.
 	//
 	// This returns the object as it appears in the DB after creation.
-	Create(context.Context, E) (E, error)
+	//
+	// An implementor may provide an empty implementation with a function that
+	// always returns an error regardless of state and input. Consult the
+	// documentation of the implementor for info.
+	Create(context.Context, M) (M, error)
 
-	Get(context.Context, ID) (E, error)
+	// Get retrieves the model with the given ID. If no entity with that ID
+	// exists, an error is returned.
+	//
+	// An implementor may provide an empty implementation with a function that
+	// always returns an error regardless of state and input. Consult the
+	// documentation of the implementor for info.
+	Get(context.Context, ID) (M, error)
 
-	GetAll(context.Context)
+	// GetAll retrieves all entities in the associated store. If no entities
+	// exist but no error otherwise occurred, the returned list of entities will
+	// have a length of zero and the returned error will be nil.
+	//
+	// An implementor may provide an empty implementation with a function that
+	// always returns an error regardless of state and input. Consult the
+	// documentation of the implementor for info.
+	GetAll(context.Context) ([]M, error)
+
+	// Update updates a particular entity in the store to match the provided
+	// model. Implementors may choose which properties of the provided value are
+	// actually used.
+	//
+	// This returns the object as it appears in the DB after updating.
+	//
+	// An implementor may provide an empty implementation with a function that
+	// always returns an error regardless of state and input. Consult the
+	// documentation of the implementor for info.
+	Update(context.Context, ID, M) (M, error)
+
+	// Delete removes the given entity from the store.
+	//
+	// This returns the object as it appeared in the DB immediately before
+	// deletion.
+	//
+	// An implementor may provide an empty implementation with a function that
+	// always returns an error regardless of state and input. Consult the
+	// documentation of the implementor for info.
+	Delete(context.Context, ID) (M, error)
+
+	// Close performs any clean-up operations required and flushes pending
+	// operations. Not all Repos will actually perform operations, but it should
+	// always be called as part of tear-down operations.
+	Close() error
+
+	// TODO: one day, move owdb Criterion functionality over and use that as a
+	// generic interface into searches. Then we can have a GetAllBy(Filter) and
+	// GetOneBy(Filter).
+}
+
+type Role int
+
+const (
+	Guest Role = iota
+	Unverified
+	Normal
+
+	Admin Role = 100
+)
+
+func (r Role) String() string {
+	switch r {
+	case Guest:
+		return "guest"
+	case Unverified:
+		return "unverified"
+	case Normal:
+		return "normal"
+	case Admin:
+		return "admin"
+	default:
+		return fmt.Sprintf("Role(%d)", r)
+	}
+}
+
+func ParseRole(s string) (Role, error) {
+	check := strings.ToLower(s)
+	switch check {
+	case "guest":
+		return Guest, nil
+	case "unverified":
+		return Unverified, nil
+	case "normal":
+		return Normal, nil
+	case "admin":
+		return Admin, nil
+	default:
+		return Guest, fmt.Errorf("must be one of 'guest', 'unverified', 'normal', or 'admin'")
+	}
+}
+
+// User is an auth model for use in the pre-rolled auth mechanism of user-in-db
+// and login identified via JWT.
+type User struct {
+	ID             uuid.UUID     // PK, NOT NULL
+	Username       string        // UNIQUE, NOT NULL
+	Password       string        // NOT NULL
+	Email          *mail.Address // NOT NULL
+	Role           Role          // NOT NULL
+	Created        time.Time     // NOT NULL
+	Modified       time.Time
+	LastLogoutTime time.Time // NOT NULL DEFAULT NOW()
+	LastLoginTime  time.Time // NOT NULL
+}
+
+func (u User) ModelID() uuid.UUID {
+	return u.ID
+}
+
+type AuthUserRepo interface {
+	Repo[uuid.UUID, User]
+
+	// Create creates a new User in the DB based on the provided one and returns
+	// the User as it appears in the DB after creation. The ID, modification
+	// time, and creation time of the provided user are ignored, as these are
+	// all automatically set; ID will be a newly-generated UUID, and both
+	// modification and creation time will be set to the current date.
+	Create(ctx context.Context, u User) (User, error)
+
+	// Get retrieves the user with the given ID. If no entity with that ID
+	// exists, an error is returned.
+	Get(ctx context.Context, id uuid.UUID) (User, error)
+
+	// GetByUsername retrieves the User with the given username. If no entity
+	// with that username exists, an error is returned.
+	GetByUsername(ctx context.Context, username string) (User, error)
+
+	// GetAll retrieves all users in the DB. If no users exist but no error
+	// otherwise occurred, the returned list of entities will have a length of
+	// zero and the returned error will be nil.
+	GetAll(ctx context.Context) ([]User, error)
+
+	// Update updates a particular user in the store to match the provided one
+	// and returns the User as it appears in the DB after the update.
+	//
+	// ID is not ignored in the provided User; this can be used to change the
+	// ID of a given User. The modification and creation time set on the
+	// provided User will be ignored, as those are automatically calculated.
+	Update(ctx context.Context, id uuid.UUID, u User) (User, error)
+
+	// Delete removes the given User from the store and returns the User as it
+	// appeared in the DB immediately before deletion.
+	Delete(ctx context.Context, id uuid.UUID) (User, error)
+
+	// Close performs any clean-up operations required and flushes pending
+	// operations.
+	Close() error
 }
--- jeldao/jelinmem/jelinmem.go
+++ jeldao/jelinmem/jelinmem.go
@@ -1,2 +1,96 @@
 // Package jelinmem provides an in-memory database for use with model types.
 package jelinmem
+
+import (
+	"fmt"
+
+	"github.com/dekarrin/jelly/jeldao"
+)
+
+type store struct {
+	repos map[string]any
+}
+
+func NewDatastore() jeldao.Store {
+	st := &store{
+		users:  NewUsersRepository(),
+		regs:   NewRegistrationsRepository(),
+		games:  NewGamesRepository(),
+		gd:     NewGameDatasRepository(),
+		seshes: NewSessionsRepository(),
+	}
+	st.coms = NewCommandsRepository(st.seshes)
+	return st
+}
+
+func (s *store) Users() jeldao.UserRepository {
+	return s.users
+}
+
+func (s *store) Registrations() jeldao.RegistrationRepository {
+	return s.regs
+}
+
+func (s *store) Games() jeldao.GameRepository {
+	return s.games
+}
+
+func (s *store) GameData() jeldao.GameDataRepository {
+	return s.gd
+}
+
+func (s *store) Sessions() jeldao.SessionRepository {
+	return s.seshes
+}
+
+func (s *store) Commands() jeldao.CommandRepository {
+	return s.coms
+}
+
+func (s *store) Close() error {
+	var err error
+	var nextErr error
+
+	nextErr = s.users.Close()
+	if nextErr != err {
+		if err != nil {
+			err = fmt.Errorf("%s\nadditionally, %w", err, nextErr)
+		} else {
+			err = nextErr
+		}
+	}
+	nextErr = s.regs.Close()
+	if nextErr != err {
+		if err != nil {
+			err = fmt.Errorf("%s\nadditionally, %w", err, nextErr)
+		} else {
+			err = nextErr
+		}
+	}
+	nextErr = s.games.Close()
+	if nextErr != err {
+		if err != nil {
+			err = fmt.Errorf("%s\nadditionally, %w", err, nextErr)
+		} else {
+			err = nextErr
+		}
+	}
+	nextErr = s.gd.Close()
+	if nextErr != err {
+		if err != nil {
+			err = fmt.Errorf("%s\nadditionally, %w", err, nextErr)
+		} else {
+			err = nextErr
+		}
+	}
+	nextErr = s.seshes.Close()
+	if nextErr != err {
+		if err != nil {
+			err = fmt.Errorf("%s\nadditionally, %w", err, nextErr)
+		} else {
+			err = nextErr
+		}
+	}
+
+	return err
+}
